0f3a66cfb508d3a318c5c9fd0b6dae280057b26a
mateyr <rodianmatey@gmail.com>
2026-09-07 19:53:34 -0600

    fix: update migrations

--- Sisprenic.Api/Database/Migrations/20260908015248_InitialCreate.Designer.cs
+++ Sisprenic.Api/Database/Migrations/20260908015248_InitialCreate.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Sisprenic.Api.Database;
 
 #nullable disable
 
-namespace sisprenic.Database.Migrations
+namespace Sisprenic.Api.Database.Migrations
 {
     [DbContext(typeof(SisprenicContext))]
-    partial class SisprenicContextModelSnapshot : ModelSnapshot
+    [Migration("20260908015248_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Sisprenic.Api/Database/Migrations/20260908015248_InitialCreate.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sisprenic.Api.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_client_identification",
+                table: "client");
+
+            migrationBuilder.AddColumn<int>(
+                name: "status",
+                table: "loan",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Estado del préstamo: 0 = Activo, 1 = Pagado");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "deleted_on",
+                table: "client",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "client",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Loan_ValidStatus",
+                table: "loan",
+                sql: "status IN (0, 1)");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_client_identification",
+                table: "client",
+                column: "identification",
+                unique: true,
+                filter: "is_deleted = false");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_client_is_deleted",
+                table: "client",
+                column: "is_deleted",
+                filter: "is_deleted = false");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Loan_ValidStatus",
+                table: "loan");
+
+            migrationBuilder.DropIndex(
+                name: "ix_client_identification",
+                table: "client");
+
+            migrationBuilder.DropIndex(
+                name: "ix_client_is_deleted",
+                table: "client");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "loan");
+
+            migrationBuilder.DropColumn(
+                name: "deleted_on",
+                table: "client");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "client");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_client_identification",
+                table: "client",
+                column: "identification",
+                unique: true);
+        }
+    }
+}
